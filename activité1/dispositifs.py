d3d10cfaa294de7977365ceede58fe10d6430901
swyftos <98902001+swyftos@users.noreply.github.com>
2025-09-15 13:04:00 +0200

    Create dispositifs.py

---
+++ activité1/dispositifs.py
@@ -0,0 +1,21 @@
+from  ampoule import Ampoule
+
+#initialisation des dispositifs
+#****************
+#   pièce 1     *
+#****************
+led1=Ampoule('led1')
+led2=Ampoule('led2')
+led3=Ampoule('led3')
+led4=Ampoule('led4')
+led5=Ampoule('led5')
+led6=Ampoule('led6')
+ampoule1=Ampoule('ampoule1')
+ampoule2=Ampoule('ampoule2')
+ampoule3=Ampoule('ampoule4')
+ampoule4=Ampoule('ampoule4')
+ampoule5=Ampoule('ampoule5')
+ruban1=Ampoule('ruban1')
+ruban2=Ampoule('ruban2')
+ruban3=Ampoule('ruban3')
+
