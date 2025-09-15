2320ae05355adf1d8e3868afd807e995591532cc
swyftos <98902001+swyftos@users.noreply.github.com>
2025-09-15 13:13:09 +0200

    Update activité_1.py

--- activité1/activité_1.py
+++ activité1/activité_1.py
@@ -1,1 +1,23 @@
+from fonctions import *
+from dispositifs import *
+
+
+allumer_tout()
+
+
+eteindre_tout()
+
+
+allumer_groupe(rubans)
+
+
+eteindre_groupe(leds_gauche)
+
+
+def etat_all():
+    print("État des luminaires :")
+    for dispositif in leds + suspension + rubans:
+        print(f"{dispositif.id} : {dispositif.etat}")
+
+etat_all()
 
