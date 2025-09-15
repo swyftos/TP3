0237422437e696921205c1638774db64e52db557
swyftos <98902001+swyftos@users.noreply.github.com>
2025-09-15 14:18:37 +0200

    Update activite_2.py

--- activité2/activite_2.py
+++ activité2/activite_2.py
@@ -1,4 +1,18 @@
-from fonctions import *
-allumer_piece("cuisine")
+from fonctions import allumer_piece
+from dispositifs import maison
+import datetime as dt
+import csv
 
+def sauvegarder_etat_csv():
+    horodatage = dt.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    lignes = []
+    for piece in maison.values():
+        for ampoule in piece:
+            lignes.append([horodatage, ampoule.id, ampoule.etat])
+    with open("etat_lum_piece1.txt", mode="a", newline="") as fichier:
+        writer = csv.writer(fichier, delimiter=",")
+        writer.writerows(lignes)
 
+# Test
+allumer_piece("Cuisine")
+sauvegarder_etat_csv()
